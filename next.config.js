fa0b4f1aa77d39226bff7f862f32f699055ae814
Daniel Vergara <danielmauriciovpinilla@hotmail.com>
2024-10-31 08:55:01 -0500

    repo

--- next.config.js
+++ next.config.js
@@ -3,8 +3,10 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    unoptimized: true,
+    unoptimized: true, // Required for GitHub Pages
   },
+  output: 'export',    // Export static files for GitHub Pages
+  basePath: '/Porttfolio.github.io
 };
 
 module.exports = nextConfig;

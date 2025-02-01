1dcd3b9cf487002f848512da4116b51cfd8f6729
DanielMauricio13 <135200493+DanielMauricio13@users.noreply.github.com>
2025-02-01 08:42:38 -0600

    hshs

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ import {motion} from 'framer-motion';
 import {fadeIn} from '../variants'
 const Home = () => {
   return(
-    <div className="bg-[#2E2E2E]/60 min-h-screen overflow-auto">
+    <div className="bg-[#2E2E2E]/60 h-full">
       <div className="w-full h-full bg-gradient-to-r from-primary/10 via-black/30 to-black-10">
         <div className="text-center flex  flex-col justify-center xl:pt-40 xl:text-left
         h-full container mx-auto">
